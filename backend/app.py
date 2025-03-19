2a1810df0eab9ad3e20e48c41788f3e9790dceff
MauricioIPastora <mauriciopastora@gmail.com>
2025-03-19 16:35:20 -0400

    added missing comma after s3 website url

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ if DEBUG:
 else:
     CORS(app, resources={r"/*": {"origins": [
         "http://localhost:5173", # local dev server
-        "http://iijs-directory-frontend.s3-website-us-east-1.amazonaws.com" #S3 website URL
+        "http://iijs-directory-frontend.s3-website-us-east-1.amazonaws.com", #S3 website URL
         "https://d26crobm8snmzc.cloudfront.net" #cloudfront distribution domain
     ]}})
 # configure database
