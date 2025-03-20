461f85aebe5e9dedb225820740ffbe5a688445bf
MauricioIPastora <mauriciopastora@gmail.com>
2025-03-20 17:53:58 -0400

    modified config.js so that mf reads env variavles correct

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
